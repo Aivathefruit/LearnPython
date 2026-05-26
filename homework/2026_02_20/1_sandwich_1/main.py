a7ff3dd395b55a1bbc694ddc39fa9077d32f4b1f
Aiva <godofnightclan@gmail.com>
2026-05-26 20:03:08 +0200

    Update main.py

--- homework/2026_02_20/1_sandwich_1/main.py
+++ homework/2026_02_20/1_sandwich_1/main.py
@@ -1,15 +1,46 @@
 def bread(func):
-    pass
+    def wrapper():
+        print("Bread")
+        func()
+        print("Bread")
+    return wrapper
 
 def salat(func):
-    pass
+    def wrapper():
+        print("Salat")
+        func()
+    return wrapper
 
 def tomato(func):
-    pass
+    def wrapper():
+        print("Tomato")
+        func()
+    return wrapper
+
+def cheese(func):
+    def wrapper():
+        print("Cheese")
+        func()
+    return wrapper
 
 def meat(func):
-    pass
+    def wrapper():
+        print("Meat")
+        func()
+    return wrapper
 
+def pickle(func):
+    def wrapper():
+        print("Pickle")
+        func()
+    return wrapper
+
+@bread
+@salat
+@pickle
+@tomato
+@cheese
+@meat
 def make_sandwich():
     pass
 
