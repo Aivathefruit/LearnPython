b0acf3f55c8f223216d29f55c2610828eecae714
Aiva <godofnightclan@gmail.com>
2026-05-30 18:25:02 +0200

    Update main.py

--- homework/2026_02_20/3_our_cache/main.py
+++ homework/2026_02_20/3_our_cache/main.py
@@ -1,5 +1,33 @@
 def cache(func):
-    pass
 
+    storage = {}
+
+    def wrapper(*args):
+        if args in storage:
+            print("[Кэш]")
+            return storage[args]
+
+        print("[Вычисление]")
+        result = func(*args)
+        storage[args] = result
+        return result
+
+    return wrapper
+
+
+@cache
 def my_sum(a, b):
     return a + b
+
+def main():
+    print(my_sum(3, 5))
+    print(my_sum(3, 5))
+    print(my_sum(10, 2))
+    print(my_sum(3, 5))
+
+if __name__ == '__main__':
+    main()
+
+
+
+
